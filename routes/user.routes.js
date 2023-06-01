09eaa4a6bc732f16b5c3f262943633c50007ba48
MSiesto <miguelsiesto@gmail.com>
2023-06-01 18:38:58 +0200

    server-side. new user routes(getAllUsers, userDetail)

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,20 @@
 const router = require("express").Router();
 const User= require('./../models/User.model')
 
+router.get("/getAllUsers", (req, res, next) => {
+    User
+    .find()
+    .then(response => res.json(response))
+    .catch(err => next(err))
+})
+
+router.get("/:_id", (req, res, next) => {
+    const {_id} = req.params
+    User.findById(_id)
+    .then(response => res.json(response))
+    .catch(err => next(err))
+})
+
 router.post("/:_id/edit", (req, res, next) => {
 
     const {username, email, role} = req.body
