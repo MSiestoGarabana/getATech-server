4044ae9e6360042ef17d1f69d606bc1d05a91192
MSiesto <miguelsiesto@gmail.com>
2023-06-09 11:49:20 +0200

    setting .eng stuff

--- db/index.js
+++ db/index.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
+require('dotenv').config();
 
 const MONGO_URI =
   process.env.MONGODB_URI 
 
+
+
 mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    const dbName = x.connections[0].name;
-    console.log(`Connected to Mongo! Database name: "${dbName}"`);
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+  .connect(process.env.MONGODB_URI)
+  .then(x => console.log(`Connected the Database: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
+
+
